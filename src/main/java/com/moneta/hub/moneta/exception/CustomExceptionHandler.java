b22bb2e9f6793f3e90656c17af6eb7ab13698170
begJosip <josipbegic57@gmail.com>
2024-01-14 23:44:39 +0100

    Add custom exception handler

---
+++ src/main/java/com/moneta/hub/moneta/exception/CustomExceptionHandler.java
@@ -0,0 +1,118 @@
+package com.moneta.hub.moneta.exception;
+
+import jakarta.mail.MessagingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.MailException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.MethodNotAllowedException;
+
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> invalidMethodArgumentException(
+            MethodArgumentNotValidException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", HttpStatus.BAD_REQUEST);
+        errors.put("errors",
+                   ex.getBindingResult().getFieldErrors().stream().map(error -> error.getField() + " " + error.getDefaultMessage())
+                     .toList());
+        errors.put("fields",
+                   ex.getBindingResult().getFieldErrors().stream().map(error -> error.getField().substring(0, 1).toUpperCase() +
+                                                                                error.getField().substring(1))
+                     .toList());
+
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Map<String, Object> illegalArgumentHandler(
+            IllegalArgumentException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", HttpStatus.BAD_REQUEST);
+        errors.put("error: ", ex.getMessage());
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(NoSuchAlgorithmException.class)
+    public Map<String, Object> noSuchAlgorithmException(
+            NoSuchAlgorithmException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        errors.put("error: ", ex.getMessage());
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public Map<String, Object> usernameNotFound(
+            UsernameNotFoundException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", HttpStatus.NOT_FOUND);
+        errors.put("error: ", ex.getMessage());
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(BadCredentialsException.class)
+    public Map<String, Object> badCredentialsException(
+            BadCredentialsException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", HttpStatus.UNAUTHORIZED);
+        errors.put("error: ", ex.getMessage());
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({MailException.class, MessagingException.class})
+    public Map<String, Object> emailErrorHandler(
+            Exception ex) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        errors.put("error: ", "Email service error.");
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public Map<String, Object> methodNotAllowedException(
+            MethodNotAllowedException ex) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", HttpStatus.METHOD_NOT_ALLOWED);
+        errors.put("error: ", ex.getMessage());
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public Map<String, Object> defaultExceptionHandler(
+            Exception ex) {
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("timestamp", LocalDateTime.now());
+        errors.put("status", HttpStatus.INTERNAL_SERVER_ERROR);
+        errors.put("error: ", ex.getMessage());
+        return errors;
+    }
+
+}
